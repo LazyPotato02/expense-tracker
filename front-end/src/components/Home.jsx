c5e5aec0b38412a2b9137907874f2616bd941902
LazyPotato <74728512+LazyPotato02@users.noreply.github.com>
2024-07-09 23:41:02 +0300

    Fixed some guards nav displays correct paths for user and anonymous user, guard checks if there is a user and it is authenticated before showing page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,31 +1,30 @@
-import { useContext, useEffect } from 'react';
-import { AuthContext } from './AuthContext';
-import { useNavigate } from 'react-router-dom';
+// import { useContext, useEffect } from 'react';
+// import { AuthContext } from './AuthContext';
+// import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-    const { auth, isCheckingAuth,logout } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (!isCheckingAuth && auth === false) {
-            navigate('/login');
-        }
-    }, [isCheckingAuth, auth, navigate]);
-
-    if (isCheckingAuth) {
-        return <div>Loading...</div>; // Or a loading spinner
-    }
-
-    if (auth === false) {
-        return null; // Or a fallback UI
-    }
+    // const { auth, isCheckingAuth,logout } = useContext(AuthContext);
+    // const navigate = useNavigate();
+    //
+    // useEffect(() => {
+    //     if (!isCheckingAuth && auth === false) {
+    //         console.log('awd')
+    //     }
+    // }, [isCheckingAuth, auth, navigate]);
+    //
+    // if (isCheckingAuth) {
+    //     return <div>Loading...</div>; // Or a loading spinner
+    // }
+    //
+    // if (auth === false) {
+    //     return null; // Or a fallback UI
+    // }
 
     return (
         <div>
             <h1>Welcome!</h1>
-            <button onClick={logout}>Logout</button>
         </div>
-    );
-};
+    )
+}
 
 export default Home;
